5c431a72b0e9bdc3020be44299b585ac007b5f7a
David Dag Vanderpuije <dabick14@gmail.com>
2020-11-09 12:12:57 +0000

    Changing Structure of Project.

---
+++ dbconfig/dbconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+class Values {
+
+
+    public static string $dbName = "trendipay";
+    public static string $host = "localhost";
+    public static string $user = "ussd";
+    public static string $password = "trendipay";
+
+
+
+
+}
+
